08707242becc50c8c6f34e843f7aa6df914a5245
Anonymous Shrimp <60944195+Anonymous-Shrimp@users.noreply.github.com>
2020-06-18 23:42:38 -0700

    STL and Code

---
+++ bluetooth.ino
@@ -0,0 +1,47 @@
+#include <SoftwareSerial.h>
+
+bool newLineS = false;
+const int relay = 2;
+String input;
+SoftwareSerial bluetooth(5, 4);
+ 
+ 
+void setup() 
+{
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, HIGH);
+}
+
+void loop()
+{
+  if(bluetooth.available()){
+  while(bluetooth.available())
+  {
+    input += bluetooth.readString();
+    delay(2);
+  }
+  Serial.println(input);
+  
+  
+  if (input == "on"){
+    
+      bluetooth.println("Turning on...");
+      digitalWrite(relay, LOW);
+    
+  }else if (input == "off"){
+    
+      bluetooth.println("Turning off...");
+      digitalWrite(relay, HIGH);
+    
+  }else{
+    bluetooth.println("Please enter on or off");
+  }
+  }
+
+  input = "";
+  
+
+  }
+  
